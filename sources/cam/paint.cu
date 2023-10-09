4b30907d9aeb2d47d20f62b464387a76e20a11a1
yoel <yridgway@student.42.fr>
2023-10-09 20:40:55 +0200

    got the tracer working on the gpu with minimal improvement

--- sources/cam/paint.cu
+++ sources/cam/paint.cu
@@ -22,63 +22,66 @@ void	my_mlx_pixel_put(t_mrt *mrt, int x, int y, int color)
 	*(unsigned int *)dst = color;
 }
 
-void __global__ ft_paint_device(t_mrt *mrt)
+void __global__ ft_paint_device(t_mrt *mrt, int size)
 {
-	// t_mrt	*mrt;
-	int		i;
-	int		j;
+	// int		j, i = 0;
 	int		color;
 
 	int index = threadIdx.x + blockIdx.x * blockDim.x;
 	int stride = blockDim.x * gridDim.x;
 	(void)index;
 	(void)stride;
-	// mrt = (t_mrt *)data;
-	i = 0;
-	printf("\nHELLO: %d\n", index);
-	printf("mrt: %p\n", mrt);
-	// mrt->ix = 1000;
-	// mrt->iy = 1800;
-	while (i < mrt->ix)
-	{
-		j = 0;
-		// ft_percentage_bar(mrt);
-		while (j < mrt->iy - 1)
-		{
-			color = get_pixel_color(mrt, i + 1, j + 1);
-			my_mlx_pixel_put(mrt, i, j, color);
-			j++;
-		}
-		i++;
-		printf("ix: %d / %d\r", i, mrt->ix);
-	}
-	// return (NULL);
-}
 
-void ft_paint_host(void *data)
+	int x, y = 0;
+	for (int i = index; i < size; i += stride)
+	{
+		// ft_percentage_bar(mrt);
+		x = i % (mrt->ix);
+		y = i / mrt->ix;
+		color = get_pixel_color(mrt, x + 1, y + 1);
+		my_mlx_pixel_put(mrt, x, y, color);
+		// i++;
+		// printf("ix: %d / %d\r", i, mrt->ix);
+	}
+}
+//     0  1  2  3
+
+// 0   0  1  2  3
+// 1   4  5  6  7
+// 2   8  9  10 11
+
+void ft_paint_host(t_mrt *mrt)
 {
-	t_mrt	*mrt;
 	int		i;
 	int		j;
+	int		size = mrt->ix * mrt->iy;
 	int		color;
 
-	mrt = (t_mrt *)data;
-	i = 0;
-	// printf("mrt: %p\n", mrt);
-	// mrt->ix = 1000;
-	// mrt->iy = 1800;
-	while (i < mrt->ix)
+	// i = 0;
+	// // printf("mrt: %p\n", mrt);
+	// while (i < mrt->ix)
+	// {
+	// 	j = 0;
+	// 	// ft_percentage_bar(mrt);
+	// 	while (j < mrt->iy - 1)
+	// 	{
+	// 		color = get_pixel_color(mrt, i + 1, j + 1);
+	// 		my_mlx_pixel_put(mrt, i, j, color);
+	// 		j++;
+	// 	}
+	// 	i++;
+	// 	// printf("ix: %d / %d\r", i, mrt->ix);
+	// }
+	int x, y = 0;
+	for (int i = 0; i < size; i += 1)
 	{
-		j = 0;
 		// ft_percentage_bar(mrt);
-		while (j < mrt->iy - 1)
-		{
-			color = get_pixel_color(mrt, i + 1, j + 1);
-			my_mlx_pixel_put(mrt, i, j, color);
-			j++;
-		}
-		i++;
-		printf("ix: %d / %d\r", i, mrt->ix);
+		x = i % (mrt->ix);
+		y = i / mrt->ix;
+		color = get_pixel_color(mrt, x + 1, y + 1);
+		my_mlx_pixel_put(mrt, x, y, color);
+		// i++;
+		// printf("ix: %d / %d\r", i, mrt->ix);
 	}
 	// return (NULL);
 }
@@ -91,17 +94,19 @@ void	pixel_calcul(t_mrt *mrt)
 	CUDA_CALL(cudaGetDevice(&deviceId));
 	CUDA_CALL(cudaDeviceGetAttribute(&numberOfSMs, cudaDevAttrMultiProcessorCount, deviceId));
 	
-	size_t threadsPerBlock = 1;//256;
-	size_t numberOfBlocks = 1;//32 * numberOfSMs;
+	size_t threadsPerBlock = 32 * 2;
+	size_t numberOfBlocks = 4 * numberOfSMs;
 	
 	// t_mrt	*dat;
 	// init_minirt(dat, "scenes/mirror_balls.rt", 1);
 	// dat = ft_copy_mrt(mrt);
 	// printf("\nHELLO\n");
-	
-	ft_paint_device<<<threadsPerBlock, numberOfBlocks>>>(mrt);
+	// CUDA_CALL(cudaMemPrefetchAsync((void *)mrt->addr, sizeof(mrt->addr), deviceId));
+	for (int i = 0; i < 10; i++)
+		ft_paint_device<<<threadsPerBlock, numberOfBlocks>>>(mrt, mrt->ix * mrt->iy);
 	CUDA_CALL(cudaGetLastError());
-	// ft_paint_host(mrt);
+	// for (int i = 0; i < 10; i++)
+		// ft_paint_host(mrt);
 	CUDA_CALL(cudaDeviceSynchronize());
 	// int		i;
 
